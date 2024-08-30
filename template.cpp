d0c04092f01a26db44169ec7c36520c720eccfc2
Ayuphys789 <Ayuphys@gmail.com>
2024-08-31 02:51:55 +0900

    Hello2

---
+++ template.cpp
@@ -0,0 +1,72 @@
+/**
+ * author: Ayuphys
+ * number:
+ * Title:
+ * Link to the Problem:
+ * Link to the Submission:
+ * Difficulty:
+ * **/
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <functional>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <numeric>
+#include <array>
+#include <random>
+#include <cstdlib>
+#include <cassert>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP(i, n) for (int i = 1; i <= (int)(n); i++)
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define rFOR(i, a, b) for (int i = a; i >= b; i--)
+#define all(x) x.begin(), x.end()
+#define fcout cout << fixed << setprecision(15)
+#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078160628620899
+const int INF = 2147483647;
+const ll INFLL = 1000000000000000000;
+const int NIL = -1;
+const int MOD = 998244353;
+// const int MOD = 1000000007;
+const ll dx[4] = {0, 1, 0, -1};
+const ll dy[4] = {1, 0, -1, 0};
+
+template <typename T>
+inline bool chmax(T &a, T b)
+{
+    return ((a < b) ? (a = b, true) : (false));
+};
+
+template <typename T>
+inline bool chmin(T &a, T b)
+{
+    return ((a > b) ? (a = b, true) : (false));
+};
+
+void Ayuphys_solve(void)
+{
+    // code
+    return;
+}
+
+int main(void)
+{
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    Ayuphys_solve();
+
+    return 0;
+}
