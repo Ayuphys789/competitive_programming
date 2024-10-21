4631e9dc8edb946bde5f334fcb83d0e37d18ad55
Ayuphys789 <Ayuphys@gmail.com>
2024-10-21 16:30:36 +0900

    add

--- atcoder/typical90/082.cpp
+++ atcoder/typical90/082.cpp
@@ -1,17 +1,59 @@
 /**
  * Author: Ayuphys
- * Problem Number:
- * Title:
- * Link to the Problem:
- * Link to the Submission:
- * Difficulty:
+ * Problem Number: typical90-082
+ * Title: Counting Numbers
+ * Link to the Problem: https://atcoder.jp/contests/typical90/tasks/typical90_cd
+ * Link to the Submission: https://atcoder.jp/contests/typical90/submissions/59038160
+ * Difficulty: 3
  */
 
 #include "/home/ayuphys789/competitive_programming/template/template.hpp"
 
+#include "/home/ayuphys789/competitive_programming/library/math/number_theory/modint.hpp"
+
+ll power_10[20];
+
+void init()
+{
+    power_10[0] = 1;
+    REP(i, 18)
+    {
+        power_10[i] = power_10[i - 1] * 10;
+    }
+    return;
+}
+
 void Ayuphys_solve(void)
 {
-    // code
+    init();
+    using mint = Fp<MOD7>;
+    ll L, R;
+    cin >> L >> R;
+    mint ans = 0;
+    for (int digit = 1; digit <= 19; ++digit)
+    {
+        if (digit == 19)
+        {
+            if (R == power_10[18])
+            {
+                ans += mint(power_10[18]) * mint(19);
+            }
+            continue;
+        }
+        ll val_left = max(L, power_10[digit - 1]);
+        ll val_right = min(R, power_10[digit] - 1);
+        if (val_left > val_right)
+        {
+            continue;
+        }
+        mint sum_left = mint(val_left - 1) * mint(val_left) / 2;
+        mint sum_right = mint(val_right) * mint(val_right + 1) / 2;
+        mint val = sum_right - sum_left;
+        mint inc = mint(digit) * mint(val);
+        ans += inc;
+        // cout << digit << ": " << val_left << "_" << val_right << "_" << inc << endl;
+    }
+    cout << ans << endl;
     return;
 }
 
