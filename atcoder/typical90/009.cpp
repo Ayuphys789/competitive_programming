618a945296043d932c6b085dec7580a4e1c846f5
Ayuphys789 <Ayuphys@gmail.com>
2025-02-25 11:31:29 +0900

    update

--- atcoder/typical90/009.cpp
+++ atcoder/typical90/009.cpp
@@ -1,23 +1,72 @@
 /**
  * Author: Ayuphys
- * Problem Number:
- * Title:
- * Link to the Problem:
- * Link to the Submission:
- * Difficulty:
+ * Problem Number: typical90-009
+ * Title: Three Point Angle
+ * Link to the Problem: https://atcoder.jp/contests/typical90/tasks/typical90_i
+ * Link to the Submission: https://atcoder.jp/contests/typical90/submissions/63134520
+ * Difficulty: 6
  */
 
 #include "/home/ayuphys789/competitive_programming/template/template.hpp"
+#include "/home/ayuphys789/competitive_programming/library/others/chmax.hpp"
+#include "/home/ayuphys789/competitive_programming/library/geometry/point.hpp"
+#include "/home/ayuphys789/competitive_programming/library/geometry/angle.hpp"
+using namespace geometry;
+
+double solve(int pos, Points &P)
+{
+    vector<double> V;
+    int N = (int)P.size();
+    rep(i, N)
+    {
+        if (i == pos)
+        {
+            continue;
+        }
+        double angle = get_angle_point(P[i] - P[pos]);
+        if (angle < 0.0)
+        {
+            angle += 2 * PI;
+        }
+        V.emplace_back(angle);
+    }
+    sort(all(V));
+    double res = 0.0;
+    int N2 = (int)V.size();
+    rep(i, N2)
+    {
+        double target = V[i] + PI;
+        if (target >= 2 * PI)
+        {
+            target -= 2 * PI;
+        }
+        int pos1 = lower_bound(all(V), target) - V.begin();
+        int cand_idx1 = pos1 % N2;
+        int cand_idx2 = (pos1 + N2 - 1) % N2;
+        double cand_angle1 = abs(V[i] - V[cand_idx1]);
+        cand_angle1 = min(cand_angle1, 2 * PI - cand_angle1);
+        double cand_angle2 = abs(V[i] - V[cand_idx2]);
+        cand_angle2 = min(cand_angle1, 2 * PI - cand_angle2);
+        chmax(res, max(cand_angle1, cand_angle2));
+    }
+    return res;
+}
 
 void Ayuphys_solve(void)
 {
     int N;
     cin >> N;
-    vector<double> X(N), Y(N);
+    Points P(N);
     rep(i, N)
     {
-        cin >> X[i] >> Y[i];
+        cin >> P[i];
     }
+    double Answer = 0.0;
+    rep(i, N)
+    {
+        chmax(Answer, solve(i, P));
+    }
+    fcout << radian_to_degree(Answer) << endl;
     return;
 }
 
--- library/geometry/angle.hpp
+++ library/geometry/angle.hpp
@@ -1,4 +1,6 @@
-#pragma onec
+#pragma once
+
+// verified with: "/atcoder/typical90/009.cpp"
 
 #include "/home/ayuphys789/competitive_programming/library/geometry/point.hpp"
 
@@ -23,5 +25,19 @@ namespace geometry
             swap(alpha, beta);
         Real theta = (beta - alpha);
         return min(theta, 2 * PI - theta);
+    }
+
+    // b を中心としてaからcに反時計回り
+    Real get_angle(const Point &a, const Point &b, const Point &c)
+    {
+        const Point v(a - b), w(c - b);
+        auto alpha = atan2(imag(v), real(v));
+        auto beta = atan2(imag(w), real(w));
+        return alpha - beta;
+    }
+
+    Real get_angle_point(const Point &a)
+    {
+        return get_angle(a, Point(0.0, 0.0), Point(1.0, 0.0));
     }
 }
